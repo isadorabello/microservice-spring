9212ee719043f7faeca11dc2c8437c2cc970974f
isadorabello <isadora.bello@gec.inatel.br>
2025-03-27 18:28:55 -0300

    feat: adding raw project - email ms

---
+++ microservice-email/src/main/java/io/github/isadorabello/microservice_email/MicroserviceEmailApplication.java
@@ -0,0 +1,13 @@
+package io.github.isadorabello.microservice_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEmailApplication.class, args);
+	}
+
+}
